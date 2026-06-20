4a58fa1d48f2580b2565a88e81bd082794288b60
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-20 11:47:05 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,28 @@ module.exports = {
         background: "#000000",
         foreground: "#ffffff",
       },
+      keyframes: {
+        slideUp: {
+          'from': { transform: 'translateY(100%)' },
+          'to': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'slide-up': 'slideUp 0.3s cubic-bezier(0.25, 1, 0.5, 1) forwards',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* IE et Edge */
+          'scrollbar-width': 'none',    /* Firefox */
+          '&::-webkit-scrollbar': {
+            'display': 'none',          /* Safari et Chrome */
+          },
+        },
+      })
+    },
+  ],
 };
