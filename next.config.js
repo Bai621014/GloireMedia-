6495b5724625a07e86c73fae2fec2040e0943a60
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-23 10:10:58 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone', // Optimisé pour les conteneurs (Render)
-  images: {
-    domains: ['placeholder-project.supabase.co'],
-  },
-};
-
-module.exports = nextConfig;
