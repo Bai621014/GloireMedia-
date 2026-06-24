5ad2455e72039b9756df7fa512a4db87aed0c3a2
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-24 07:59:25 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="p-8 text-center">
+      <h1 className="text-2xl font-bold">Bienvenue sur GloireMedia</h1>
+      <p>La plateforme du Pasteur Chris.</p>
+    </main>
+  );
+}
