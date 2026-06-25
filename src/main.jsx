3d41d5a81e8c31ba4abc5f0f0f2befffacd4ee81
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-25 11:52:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import UserProfile from './UserProfile';
-import './style.css';
+// Si vous avez un fichier CSS, gardez cette ligne. Sinon, supprimez-la.
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
